11dd7a7844c3aac30425ae05899a61cfddf6d609
keko <96478989+lex0tan@users.noreply.github.com>
2022-11-27 02:45:12 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
 window.onscroll = e => {
     let element = document.getElementById("nav");
     let logo = document.getElementById("logo");
@@ -27,34 +31,102 @@ window.onscroll = e => {
 }
 
 
-
+let txt = document.getElementById("errormsg");
 let btn = document.getElementById("submitBtn");
 btn.addEventListener("click", event => {
-    event.preventDefault();
+    btn.innerHTML = "<div class='spinner-grow spinner-grow-sm' role='status'>";
     let instagram = document.getElementById("instagram").value;
     let email = document.getElementById("email").value;
     let name = document.getElementById("name").value;
     let message = document.getElementById("msg").value;
+    
+    if (instagram == "" || email == "" || name == "") {
+        txt.innerHTML = "Please fill in all the fields";
+        txt.style.color = "red";
+        btn.innerHTML = "submit";
+        return
+    }
+    
     let data = {
         instagram,
         email,
         name,
-        message
-    };
-    fetch("https://blame.pythonanywhere.com", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then(res => {
-        console.log(res);
-        if (res.status == 200) {
-            alert("Message sent successfully!");
-        }
-    }).catch(err => {
-        console.log(err);
-    });
-});
-    
+        message};
+        
+        fetch("https://blame.pythonanywhere.com", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res => {
+            if (res.status == 200) {
+                btn.style.animation = "onsuccess 2s"
+                sleep(2000).then(() => {
+                    btn.innerHTML = "<img src='https://img.icons8.com/ios-filled/25/null/ok--v1.png'style='margin-right: 5px;'/>Thank you"
+                    btn.classList.add("success")
+                })}
 
+            else if (res.status != 200) {
+                btn.style.animation = "onerror 2s";
+                sleep(2000).then(() => {
+                    btn.innerHTML = "<img src='https://img.icons8.com/ios-filled/25/null/error--v1.png'style='margin-right: 5px;'/>Error"
+                    btn.classList.add("error")
+                    txt.style.color = "red";
+                });
+            };
+
+            }).catch(err => {
+                btn.style.animation = "onerror 2s";
+                sleep(2000).then(() => {
+                    btn.innerHTML = "<img src='https://img.icons8.com/ios-filled/25/null/error--v1.png'style='margin-right: 5px;'/>Error"
+                    btn.classList.add("error")
+                    txt.style.color = "red";
+                }); 
+        })
+})
+
+document.getElementById('instagram').addEventListener('change', pressHandler =>{
+    let input = document.getElementById('instagram').value;
+    let regex = /([a-zA-Z0-9_.]+)/;
+    if (regex.test(input) == false){
+        console.log("ig")
+        document.getElementById('errormsg').innerHTML = "Please enter a valid instagram link";
+        document.getElementById('errormsg').style.color = "red";
+        document.getElementById('submitBtn').disabled = true;
+    }
+    else {
+        document.getElementById('errormsg').innerHTML = "";
+        document.getElementById('submitBtn').disabled = false;
+    }
+});
+
+document.getElementById('email').addEventListener('change', pressHandler =>{
+    let input = document.getElementById('email').value;
+    let regex = /([a-zA-Z0-9_.]+@[a-zA-Z0-9_.]+)/;
+    if (regex.test(input) == false){
+        console.log("email")
+        document.getElementById('errormsg').innerHTML = "Please enter a valid email";
+        document.getElementById('errormsg').style.color = "red";
+        document.getElementById('submitBtn').disabled = true;
+    }
+    else {
+        document.getElementById('errormsg').innerHTML = "";
+        document.getElementById('submitBtn').disabled = false;
+    }
+})
+
+document.getElementById('name').addEventListener('change', pressHandler =>{
+    let input = document.getElementById('name').value;
+    let regex = /([a-zA-Z0-9_.]+)/;
+    if (regex.test(input) == false){
+        console.log("name")
+        document.getElementById('errormsg').innerHTML = "Please enter a valid name";
+        document.getElementById('errormsg').style.color = "red";
+        document.getElementById('submitBtn').disabled = true;
+    }
+    else {
+        document.getElementById('errormsg').innerHTML = "";
+        document.getElementById('submitBtn').disabled = false;
+    }
+});
